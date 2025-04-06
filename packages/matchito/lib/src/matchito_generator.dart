c4e03e9b053b2644c67fb35c5f06b98930a2d820
mrjosch <mrjosch99@gmail.com>
2025-04-06 23:15:20 +0200

    fix generator

--- packages/matchito/lib/src/matchito_generator.dart
+++ packages/matchito/lib/src/matchito_generator.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
@@ -14,7 +13,7 @@ class MatchitoGenerator extends GeneratorForAnnotation<Matchito> {
     Element element,
     ConstantReader annotation,
     BuildStep buildStep,
-  ) async* {
+  ) {
     final matchitoAnnotation = annotation.peek('types');
     if (matchitoAnnotation == null || !matchitoAnnotation.isList) {
       throw InvalidGenerationSourceError(
@@ -42,65 +41,59 @@ class MatchitoGenerator extends GeneratorForAnnotation<Matchito> {
       );
     }
 
-    yield 'const _sentinel = Object();';
-    for (final type in types) {
-      try {
-        final typeName = type.getDisplayString();
-        final typeElement = type.element as ClassElement?;
-        final parameters = {
-          ...?typeElement?.fields
-              .where(
-                (field) =>
-                    !field.isStatic &&
-                    field.name != 'copyWith' &&
-                    field.name != 'hashCode' &&
-                    field.name != 'runtimeType' &&
-                    !field.name.startsWith('_'),
-              )
-              .map((field) => field.name),
-          ...?typeElement?.allSupertypes
-              .expand((supertype) => supertype.element.fields)
-              .where(
-                (field) =>
-                    !field.isStatic &&
-                    field.name != 'copyWith' &&
-                    field.name != 'hashCode' &&
-                    field.name != 'runtimeType' &&
-                    !field.name.startsWith('_'),
-              )
-              .map((field) => field.name),
-        };
+    final buffer = ['const _sentinel = Object();\n'];
 
-        if (parameters.isEmpty) {
-          yield 'Matcher is$typeName() => isA<$typeName>();';
-        } else {
-          final parameterList = parameters
-              .map((param) => 'Object? $param = _sentinel')
-              .join(', ');
-          final parameterMatchers = parameters
-              .map((param) {
-                return '''
+    for (final type in types) {
+      final typeName = type.getDisplayString();
+      final typeElement = type.element as ClassElement?;
+      final parameters = {
+        ...?typeElement?.fields
+            .where(
+              (field) =>
+                  !field.isStatic &&
+                  field.name != 'copyWith' &&
+                  field.name != 'hashCode' &&
+                  field.name != 'runtimeType' &&
+                  !field.name.startsWith('_'),
+            )
+            .map((field) => field.name),
+        ...?typeElement?.allSupertypes
+            .expand((supertype) => supertype.element.fields)
+            .where(
+              (field) =>
+                  !field.isStatic &&
+                  field.name != 'copyWith' &&
+                  field.name != 'hashCode' &&
+                  field.name != 'runtimeType' &&
+                  !field.name.startsWith('_'),
+            )
+            .map((field) => field.name),
+      };
+
+      if (parameters.isEmpty) {
+        buffer.add('Matcher is$typeName() => isA<$typeName>();\n');
+      } else {
+        final parameterList = parameters
+            .map((param) => 'Object? $param = _sentinel')
+            .join(', ');
+        final parameterMatchers = parameters
+            .map((param) {
+              return '''
       if ($param != _sentinel) {
         matcher = matcher.having((e) => e.$param, '$param', $param);
       }
     ''';
-              })
-              .join('\n');
+            })
+            .join('\n');
 
-          yield 'Matcher is$typeName({$parameterList}) {';
-          stderr.writeln('Matcher is$typeName({$parameterList}) {');
-          yield '  var matcher = isA<$typeName>();';
-          stderr.writeln('  var matcher = isA<$typeName>();');
-          yield parameterMatchers;
-          stderr.writeln(parameterMatchers);
-          yield '  return matcher;';
-          stderr.writeln('  return matcher;');
-          yield '}';
-          stderr.writeln('}');
-        }
-      } catch (e) {
-        stderr.writeln(e.toString());
+        buffer.add('Matcher is$typeName({$parameterList}) {');
+        buffer.add('  var matcher = isA<$typeName>();\n');
+        buffer.add(parameterMatchers);
+        buffer.add('  return matcher;');
+        buffer.add('}');
       }
     }
+
+    return Stream.value(buffer.join('\n'));
   }
 }
